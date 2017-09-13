e2e8544fec56348ffb90e080eb884899f5c5b16c
aavolkova <avolkova@montgomerycollege.edu>
2017-09-13 16:00:46 -0400

    added CRN to the Course

--- src/main/java/com/finalproject/courseevaluation_studentattendance/Model/Course.java
+++ src/main/java/com/finalproject/courseevaluation_studentattendance/Model/Course.java
@@ -1,10 +1,7 @@
 package com.finalproject.courseevaluation_studentattendance.Model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -13,6 +10,8 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    private String crn;
 
     private String courseName;
 
@@ -29,6 +28,14 @@ public class Course {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getCrn() {
+        return crn;
+    }
+
+    public void setCrn(String crn) {
+        this.crn = crn;
     }
 
     public String getCourseName() {
@@ -62,4 +69,7 @@ public class Course {
     public void setStatus(boolean status) {
         this.status = status;
     }
+
+
+
 }
