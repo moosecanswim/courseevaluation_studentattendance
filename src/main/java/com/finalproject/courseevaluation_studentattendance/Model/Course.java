3995444e6fca4e3740dadf06e6249ee2d55738ee
aavolkova <avolkova@montgomerycollege.edu>
2017-09-13 14:25:10 -0400

    Course updated: fields added, getters and setters

--- src/main/java/com/finalproject/courseevaluation_studentattendance/Model/Course.java
+++ src/main/java/com/finalproject/courseevaluation_studentattendance/Model/Course.java
@@ -1,4 +1,65 @@
 package com.finalproject.courseevaluation_studentattendance.Model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String courseName;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private boolean status;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
