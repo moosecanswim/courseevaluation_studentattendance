916083cb0131a79d8d7b81ccc34db1ca427b8f53
gracelf <grace.luning.fu@gmail.com>
2017-09-13 17:53:22 -0400

    teacher home page done, working on course detail page(mark attendance,list students etc)

--- src/main/java/com/finalproject/courseevaluation_studentattendance/Controller/TeacherController.java
+++ src/main/java/com/finalproject/courseevaluation_studentattendance/Controller/TeacherController.java
@@ -1,13 +1,59 @@
 package com.finalproject.courseevaluation_studentattendance.Controller;
 
+import com.finalproject.courseevaluation_studentattendance.Model.Course;
+import com.finalproject.courseevaluation_studentattendance.Model.Person;
+import com.finalproject.courseevaluation_studentattendance.Repositories.CourseRepository;
+import com.finalproject.courseevaluation_studentattendance.Repositories.PersonRepository;
+import com.finalproject.courseevaluation_studentattendance.Services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.websocket.server.PathParam;
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
+
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
     @RequestMapping("/home")
     public String teacherHome(){
         return "teacherpages/teacherhome";
+    }
+
+
+    //this route can be combine with the teacher home page later
+    @GetMapping("/listallcourses")
+    public String listCourse(Principal p, Model model)
+    {
+        Person instructor = personRepository.findByUsername(p.getName());
+
+        Iterable<Course> allCoursesofAInstructor = instructor.getCourseInstructor();
+
+        model.addAttribute("allcoursesofaTeacher", allCoursesofAInstructor);
+
+        return "listallcourses";
+    }
+
+
+    //will have a add student
+    @GetMapping("/detailsofacourse/{id}")
+    public String detailsofcourse(@PathParam("id") Long courseId, Model model)
+    {
+
+        Course currentCourse = courseRepository.findOne(courseId);
+        model.addAttribute("course", currentCourse);
+
+        return "detailsofacourse";
     }
 
 }
