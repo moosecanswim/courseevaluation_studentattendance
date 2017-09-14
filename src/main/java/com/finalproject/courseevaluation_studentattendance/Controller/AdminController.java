8ff85ca7cca1f505517f37a316e89f5e94582285
aavolkova <avolkova@montgomerycollege.edu>
2017-09-14 12:03:34 -0400

    admincoursedatails: added remove student route (need to be modified)

--- src/main/java/com/finalproject/courseevaluation_studentattendance/Controller/AdminController.java
+++ src/main/java/com/finalproject/courseevaluation_studentattendance/Controller/AdminController.java
@@ -69,7 +69,7 @@ public class AdminController {
         return "adminpages/addcourse";
     }
 
-    @GetMapping("/detailsofacourse/{id}")
+    @GetMapping("/admincourcedatails/{id}")  //id - course id
     public String displayCourse (@PathVariable("id")long id,
                                          Model model) {
 
@@ -79,13 +79,11 @@ public class AdminController {
         Set<Person> courseInstructors = currentCourse.getInstructor();
         model.addAttribute("courseInstructors", courseInstructors);
 
-        Set<Evaluation> courseEvaluations = currentCourse.getEvaluations();
-        model.addAttribute("courseEvaluations", courseEvaluations);
 
         Set<Person> courseStudents = currentCourse.getStudent();
         model.addAttribute("courseStudents", courseStudents);
 
-        return "adminpages/datailsofacourse";
+        return "adminpages/admincourcedatails";
     }
 
     //need to taste viewing after the team is done with evaluation
@@ -96,6 +94,17 @@ public class AdminController {
         model.addAttribute("evaluation",thiscourseevaluation);
         model.addAttribute("course",thiscourse);
         return"courseevaluation";
+    }
+
+    // ===   Remove Student from the Course
+    @GetMapping("course/{courseid}/removestudentfromcourse/{studentid}")
+    public String removeStudentFromCourse (@PathVariable("courseid")long courseid,
+                                           @PathVariable("studentid")long studentid,
+                                           Model model)
+    {
+//        todo: call method here
+        String courseIDString = Long.toString(courseid);
+        return "rdirect:/admin/detailsofacourse/" + courseIDString;
     }
 
 
