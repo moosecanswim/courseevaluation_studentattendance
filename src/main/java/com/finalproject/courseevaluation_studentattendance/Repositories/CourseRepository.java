5b2ae0cb0f138fda03ad040e4e6ed9784694ffed
mahderf <mymahder@gmail.com>
2017-09-14 11:51:20 -0400

    admin can add students to course

--- src/main/java/com/finalproject/courseevaluation_studentattendance/Repositories/CourseRepository.java
+++ src/main/java/com/finalproject/courseevaluation_studentattendance/Repositories/CourseRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface CourseRepository extends CrudRepository<Course,Long> {
    Iterable<Course> findAllByCourseName(String courseName);
 
+
 }
