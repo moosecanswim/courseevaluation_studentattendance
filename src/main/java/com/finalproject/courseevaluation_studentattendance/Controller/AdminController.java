0b41b2d470d95f92f79e6496866c83bf38d8986d
aavolkova <avolkova@montgomerycollege.edu>
2017-09-15 19:50:11 -0400

    addperson html created

--- src/main/java/com/finalproject/courseevaluation_studentattendance/Controller/AdminController.java
+++ src/main/java/com/finalproject/courseevaluation_studentattendance/Controller/AdminController.java
@@ -262,4 +262,25 @@ public class AdminController {
         return "adminpages/admineditpeople";
     }
 
+
+
+    // admin create new person
+    @GetMapping("/addperson")
+    public String addPerson(Model model)
+    {
+        model.addAttribute("person", new Person());
+        return "adminpages/adminaddperson";
+    }
+
+    // Validate entered information and if it is valid display the result
+    // Person must have first name, last name, and email address
+    @PostMapping("/addperson")
+    public String postPerson(@ModelAttribute("person") Person person)
+    {
+        personRepo.save(person);
+        return"redirect:/admin/home";
+    }
+
+
+
 }
