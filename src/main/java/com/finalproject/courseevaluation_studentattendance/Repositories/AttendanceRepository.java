875eaa70ca9d5184a4fdfc7e6abe3e1ea98b197d
gracelf <grace.luning.fu@gmail.com>
2017-09-17 20:55:20 -0400

    enable attendance update(tested) update an attendance status if already exists for the same student, same course, same day

--- src/main/java/com/finalproject/courseevaluation_studentattendance/Repositories/AttendanceRepository.java
+++ src/main/java/com/finalproject/courseevaluation_studentattendance/Repositories/AttendanceRepository.java
@@ -1,7 +1,12 @@
 package com.finalproject.courseevaluation_studentattendance.Repositories;
 
 import com.finalproject.courseevaluation_studentattendance.Model.Attendance;
+import com.finalproject.courseevaluation_studentattendance.Model.Course;
+import com.finalproject.courseevaluation_studentattendance.Model.Person;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AttendanceRepository extends CrudRepository<Attendance,Long> {
+
+
+    Attendance findAllByAttendanceCourseEqualsAndDateEqualsAndPersonAttendanceEquals(Course c, String d, Person student);
 }
